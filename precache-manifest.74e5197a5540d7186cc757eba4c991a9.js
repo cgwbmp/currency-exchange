a106c7e85a99aaf15051592e28891356654ea058
Alena <cgwbmp@gmail.com>
2019-11-03 12:59:42 +0500

    Updates

---
+++ precache-manifest.74e5197a5540d7186cc757eba4c991a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef83d249bf7dfe7ed6a0e16e21de2c88",
+    "url": "/currency-exchange/index.html"
+  },
+  {
+    "revision": "e6ece1eccae219fcde90",
+    "url": "/currency-exchange/static/css/2.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "83a57c38a3bc872f2ed4",
+    "url": "/currency-exchange/static/css/main.2d71931b.chunk.css"
+  },
+  {
+    "revision": "e6ece1eccae219fcde90",
+    "url": "/currency-exchange/static/js/2.19107610.chunk.js"
+  },
+  {
+    "revision": "83a57c38a3bc872f2ed4",
+    "url": "/currency-exchange/static/js/main.c3268bb4.chunk.js"
+  },
+  {
+    "revision": "09a0f7784f0c1d818c71",
+    "url": "/currency-exchange/static/js/runtime-main.ade7e3dc.js"
+  }
+]);
